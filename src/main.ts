b1dc69b661e55607f7a0c0a6190681237292b996
tikisailor <40813246+tikisailor@users.noreply.github.com>
2022-02-08 18:48:26 +0800

    initial setup: set http server, make .env available, set port and global prefix from .env, add validation pipeline, add swagger.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,49 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { VersioningType, ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // App setup
+  const app: NestFastifyApplication = await NestFactory.create(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  // Makes .env available
+  const configService = app.get(ConfigService);
+
+  // // Get global prefix from .env
+  const globalPrefix: string = configService.get('GLOBAL_PREFIX');
+
+  // Get port number from .env file
+  const port: number = configService.get('PORT');
+
+  // Versioning system
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
+  // Set global prefix
+  app.setGlobalPrefix(globalPrefix);
+
+  // Enable validation pipeline globally
+  app.useGlobalPipes(new ValidationPipe());
+
+  // Swagger setup
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Polkawatch Live Query Sever')
+    .setDescription('Nestjs api for Polkawatch elastic indexer')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup(globalPrefix, app, document);
+
+  await app.listen(port);
 }
 bootstrap();
