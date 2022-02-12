fe01796cd122f7ae2ea02e0b48e5b734e49d351c
rafael <rvalle@privaz.io>
2022-02-12 13:08:17 +0100

    facilitate application configuration sharing between testing and production execution modes

--- src/main.ts
+++ src/main.ts
@@ -3,26 +3,29 @@ import { AppModule } from './lqs.module';
 import { ConfigService } from '@nestjs/config';
 import { VersioningType, ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { OpenAPI } from 'openapi-types';
+
 import {
     FastifyAdapter,
     NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 
-async function bootstrap() {
-    // App setup
-    const app: NestFastifyApplication = await NestFactory.create(
-        AppModule,
-        new FastifyAdapter(),
-    );
+
+/**
+ * Configures the Application. Allows to share configuration between Production
+ * and Test runs.
+ *
+ * Returns the Swagge Document which is also required for testing.
+ *
+ * @param app
+ */
+export function configure(app, setupSwaggerModule=true): OpenAPI.Document{
 
     // Makes .env available
     const configService = app.get(ConfigService);
 
     // // Get global prefix from .env
     const globalPrefix: string = configService.get('LQS_GLOBAL_PREFIX');
-
-    // Get port number from .env file
-    const port: number = configService.get('LQS_PORT');
 
     // Versioning system
     app.enableVersioning({
@@ -37,13 +40,25 @@ async function bootstrap() {
 
     // Swagger setup
     const swaggerConfig = new DocumentBuilder()
-        .setTitle('Polkawatch Live Query Sever')
-        .setDescription('REST API for Polkawatch indexer')
-        .setVersion('1.0')
-        .build();
+      .setTitle('Polkawatch Live Query Sever')
+      .setDescription('REST API for Polkawatch indexer')
+      .setVersion('1.0')
+      .build();
     const document = SwaggerModule.createDocument(app, swaggerConfig);
-    SwaggerModule.setup(globalPrefix, app, document);
+    if(setupSwaggerModule) SwaggerModule.setup(globalPrefix, app, document);
 
-    await app.listen(port);
+    return document as OpenAPI.Document;
+}
+
+async function bootstrap() {
+    // App setup
+    const app: NestFastifyApplication = await NestFactory.create(
+        AppModule,
+        new FastifyAdapter(),
+    );
+
+    configure(app);
+
+    await app.listen(app.get(ConfigService).get('LQS_PORT'));
 }
 bootstrap();
