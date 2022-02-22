d7f6d6fe37cb3eda27eaa75fde2df3851d65da6c
tikisailor <40813246+tikisailor@users.noreply.github.com>
2022-02-22 22:00:48 +0800

    add .eslintignore and lint files

--- packages/archive/src/index.ts
+++ packages/archive/src/index.ts
@@ -1,7 +1,7 @@
-//Exports all handler functions
-export * from './mappings/Heartbeat'
-export * from './mappings/TimeStamp'
-export * from './mappings/Reward'
-export * from './mappings/Payout'
+// Exports all handler functions
+export * from './mappings/Heartbeat';
+export * from './mappings/TimeStamp';
+export * from './mappings/Reward';
+export * from './mappings/Payout';
 
-import "@polkadot/api-augment"
+import '@polkadot/api-augment';
