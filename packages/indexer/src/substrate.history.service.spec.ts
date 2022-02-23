be5d293acec2f4284b8b2d00729f98a1f05361e7
rafael <rvalle@privaz.io>
2022-02-23 18:35:33 +0100

    added required configuration to the test

--- packages/indexer/src/substrate.history.service.spec.ts
+++ packages/indexer/src/substrate.history.service.spec.ts
@@ -1,12 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { SubstrateHistoryService } from './substrate.history.service';
+import { SubstrateHistoryService, SubstrateAPIService } from './substrate.history.service';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+
 
 describe('SubstrateService', () => {
     let service: SubstrateHistoryService;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            providers: [SubstrateHistoryService],
+            imports: [
+                ConfigModule.forRoot({
+                    isGlobal: true,
+                    validationSchema: Joi.object({
+                        INDEXER_SUBSTRATE_RPC_URL: Joi.string().default('wss://polkadot.valletech.eu'),
+                    }),
+                }),
+            ],
+            providers: [SubstrateAPIService, SubstrateHistoryService],
         }).compile();
 
         service = module.get<SubstrateHistoryService>(SubstrateHistoryService);
