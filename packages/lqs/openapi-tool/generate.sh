374b90b44bfcfd055126fd512863ead9ad5393f9
rafael <rvalle@privaz.io>
2022-03-05 08:07:53 +0100

    moved into the dockerfile as default command, that can be overidden

--- packages/lqs/openapi-tool/generate.sh
+++
@@ -1,5 +0,0 @@
-openapi-generator-cli generate \
-    -i /local/api-spec.json \
-    --generator-name typescript-axios \
-    -o /local/out \
-    --additional-properties=useSingleRequestParameter=true
