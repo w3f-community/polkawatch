3123f3c7c1e511201d0564c1d0a717d8e209a1d6
rafael <rvalle@privaz.io>
2022-03-09 11:04:26 +0100

    added bundled sample

--- packages/ddp/src/app.controller.ts
+++ packages/ddp/src/app.controller.ts
@@ -1,31 +1,35 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
-import {Configuration, GeographyApi} from "@lqs/client";
-
+import {Configuration,
+  GeographyApi,
+  ValidatorApi,
+  NetworkApi,
+  RewardDistributionQueryDto
+} from "@lqs/client";
 
 // TODO: here we have to test the required features:
-// - compression: responses are compressed and served from .gz urls
-// - bundling: a single endpoint returns several calls
+// - bundling: a single endpoint returns several calls OK
 // - transformation: for charting libraries
 // - dpp client for use from react
 // - client can connect to dpp or IPFS holding same contents
+// - compression: responses are compressed and served from .gz urls (OPTIONAL)
 
 @Controller()
 export class AppController {
 
-  private api;
+  private readonly conf;
 
-  constructor(private readonly appService: AppService) {
-  }
-
-  @Get("test.gz")
-  async getHello(): Promise<any> {
-
-    const api=new GeographyApi(new Configuration({
+  constructor() {
+    this.conf=new Configuration({
       //TODO: how to make the cert work
       //basePath: "https://pwlqs.vtk.lan"
       basePath: "http://localhost:7000"
-    }));
+    });
+  }
+
+  @Get("test")
+  async getTest(): Promise<any> {
+
+    let api=new GeographyApi(this.conf);
 
     let countries= await api.geoCountryControllerPost({
       rewardDistributionQueryDto:{
@@ -35,6 +39,29 @@ export class AppController {
     });
 
     return countries.data ;
-    //return this.appService.getHello()
   }
+
+  @Get("bundle")
+  async getBundle(): Promise<any>{
+    const api=new GeographyApi(this.conf);
+
+    const params:RewardDistributionQueryDto = {
+      StartingEra: 510,
+      TopResults: 10
+    };
+
+    const regions = await api.geoRegionControllerPost({
+      rewardDistributionQueryDto: params}
+    );
+
+    const countries = await api.geoCountryControllerPost({
+      rewardDistributionQueryDto: params}
+    );
+
+    return {
+      regions:regions.data,
+      countries:countries.data,
+    };
+  }
+
 }
